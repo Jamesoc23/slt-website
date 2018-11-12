ce22b6d272863d483c0826847444012bb92cf92e
Jamesoc23 <james.oc23@hotmail.com>
2018-11-12 15:34:48 +0000

    Added deployment files for deploying to Heroku

--- wexfordslt/settings.py
+++ wexfordslt/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,12 @@ MEDIA_URL = '/slt/'
 MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'slt/')
 )
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
