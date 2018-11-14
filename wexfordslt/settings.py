af2bda58967a7596f29faaf9d7ee3d5fd04c63ab
Jamesoc23 <james.oc23@hotmail.com>
2018-11-14 12:01:38 +0000

    changed static root

--- wexfordslt/settings.py
+++ wexfordslt/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'slt/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'slt/static'),
 )
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
