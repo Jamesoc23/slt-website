8a4d59dcd4043b1d2b516542fadba1facf6ac37f
Jamesoc23 <james.oc23@hotmail.com>
2018-11-14 16:31:21 +0000

    changed static root

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wexfordslt.wsgi --log-file -
+web: gunicorn wexfordslt.wsgi --log-file collectstatic
--- wexfordslt/settings.py
+++ wexfordslt/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'slt/static/slt'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'slt/static'),
 )
