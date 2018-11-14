f66ae2e2a11ee0235c7590de7af41ea90c34bc6d
Jamesoc23 <james.oc23@hotmail.com>
2018-11-14 11:25:27 +0000

    added static to root directory

--- wexfordslt/settings.py
+++ wexfordslt/settings.py
@@ -123,12 +123,10 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '../slt/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../slt/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
